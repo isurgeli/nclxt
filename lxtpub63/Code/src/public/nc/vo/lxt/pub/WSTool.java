f7701ca5c3edb7cbc43b17355ee0d3a888294a10
isurge <isurgeli@gmail.com>
2013-12-05 17:00:32 +0800

    修正WS认证访问。

--- lxtpub63/Code/src/public/nc/vo/lxt/pub/WSTool.java
+++ lxtpub63/Code/src/public/nc/vo/lxt/pub/WSTool.java
@@ -4,14 +4,19 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
+import java.net.URL;
 
 import nc.vo.pub.BusinessException;
 
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.UsernamePasswordCredentials;
+import org.apache.commons.httpclient.auth.AuthScope;
 import org.apache.commons.httpclient.methods.InputStreamRequestEntity;
 import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.commons.httpclient.methods.RequestEntity;
+
+
 
 
 public class WSTool {
@@ -21,7 +26,7 @@ public class WSTool {
 			// 然后把Soap请求数据添加到PostMethod中
 			byte[] b = soapMsg.getBytes("utf-8");
 			InputStream is = new ByteArrayInputStream(b, 0, b.length);
-			RequestEntity re = new InputStreamRequestEntity(is, b.length, "application/soap+xml; charset=utf-8");
+			RequestEntity re = new InputStreamRequestEntity(is, b.length, "text/xml;charset=utf-8");
 			postMethod.setRequestEntity(re);
 	
 			// 最后生成一个HttpClient对象，并发出postMethod请求
@@ -40,5 +45,38 @@ public class WSTool {
 		} catch (IOException e) {
 			throw new BusinessException(e);
 		}
+	}
+	
+	public static String callByHttpBasicAuth(String urlStr, String soapMsg, String user, String pass) throws BusinessException {
+		try {
+			URL url = new URL(urlStr);
+			PostMethod postMethod = new PostMethod(urlStr);
+
+			byte[] b = soapMsg.getBytes("utf-8");
+			InputStream is = new ByteArrayInputStream(b, 0, b.length);
+			RequestEntity re = new InputStreamRequestEntity(is, b.length, "text/xml;charset=utf-8");
+			postMethod.setRequestEntity(re);
+			
+			HttpClient httpClient = new HttpClient();
+			httpClient.getParams().setAuthenticationPreemptive(true);
+			httpClient.getState().setCredentials(new AuthScope(url.getHost(), url.getPort(), AuthScope.ANY_REALM), 
+					new UsernamePasswordCredentials(user, pass));
+			httpClient.getHttpConnectionManager().getParams().setConnectionTimeout(10000);  
+			httpClient.getHttpConnectionManager().getParams().setSoTimeout(10000); 
+			int statusCode = httpClient.executeMethod(postMethod);
+			if (statusCode == 200) {
+				String soapResponseData = postMethod.getResponseBodyAsString();
+				return soapResponseData;
+			} else {
+				throw new BusinessException("错误 HTTP "+statusCode);
+			}
+		} catch (UnsupportedEncodingException e) {
+			throw new BusinessException(e);
+		} catch (HttpException e) {
+			throw new BusinessException(e);
+		} catch (IOException e) {
+			throw new BusinessException(e);
+		}
+		
 	}
 }
---
+++ lxtpub63/Code/src/test/nc/vo/lxt/pub/test/WSToolTest.java
@@ -0,0 +1,53 @@
+package nc.vo.lxt.pub.test;
+
+import junit.framework.TestCase;
+import nc.vo.lxt.pub.WSTool;
+import nc.vo.pub.BusinessException;
+
+public class WSToolTest extends TestCase {
+	public WSToolTest(String name) {
+		super(name);
+	}
+
+	public void testWSAuth() throws BusinessException {
+		WSTool.callByHttpBasicAuth(
+				"http://erpdev.crmsc.com.cn:8000/sap/bc/srt/rfc/sap/zws_fkdjk/206/zws_fkdjk/zws_fkdjk",
+				getSoapMsg(), "zhoujs", "zhouman");
+
+	}
+
+	private String getSoapMsg() {
+		StringBuffer soap = new StringBuffer();
+		soap.append("<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:urn=\"urn:sap-com:document:sap:soap:functions:mc-style\">");
+		soap.append("   <soapenv:Header/>");
+		soap.append("   <soapenv:Body>");
+		soap.append("      <urn:ZfiFkdjk>");
+		soap.append("         <!--Optional:-->");
+		soap.append("         <IAedat>");
+		soap.append("            <!--Zero or more repetitions:-->");
+		soap.append("            <item>");
+		soap.append("               <Sign>I</Sign>");
+		soap.append("               <Option>BT</Option>");
+		soap.append("               <Low>20130101</Low>");
+		soap.append("               <High>20131231</High>");
+		soap.append("            </item>");
+		soap.append("         </IAedat>");
+		soap.append("         <!--Optional:-->");
+		soap.append("         <IBukrs>");
+		soap.append("            <!--Zero or more repetitions:-->");
+		soap.append("            <item>");
+		soap.append("               <Sign>I</Sign>");
+		soap.append("               <Option>BT</Option>");
+		soap.append("               <Low>0001</Low>");
+		soap.append("               <High>2300</High>");
+		soap.append("            </item>");
+		soap.append("         </IBukrs>");
+		soap.append("       <OTab>");
+		soap.append("         </OTab>");
+		soap.append("      </urn:ZfiFkdjk>");
+		soap.append("   </soapenv:Body>");
+		soap.append("</soapenv:Envelope>");
+
+		return soap.toString();
+	}
+}
